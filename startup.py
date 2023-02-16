467c2d6921ef4ff17c013fe11ef72c1251b76169
BadPingHere <62813557+BadPingHere@users.noreply.github.com>
2023-02-16 17:22:57 -0500

    im so happy i got rid of password leak

--- startup.py
+++ startup.py
@@ -1,45 +1,37 @@
 import time
-from pywinauto.application import Application
 from pywinauto import application
 import pywinauto.keyboard
 import pywinauto.mouse
 import pywinauto.base_wrapper
 import pywinauto.timings
-import os
-app = application.Application()
-
-try:    app.start('C:\\Program Files\\Mozilla Firefox\\firefox.exe')
-except: # Failed
-    a = "Firefox failed to open!"
-else:  # Success
-    time.sleep(1)
-    pywinauto.mouse.click(button='left', coords=(200, 60))
-    pywinauto.keyboard.send_keys("youtube.com") # adds first tab
-    pywinauto.keyboard.send_keys("{ENTER}")
-    pywinauto.mouse.click(button='left', coords=(245, 20))# clicks new tab button
-    pywinauto.mouse.click(button='left', coords=(200, 60))
-    pywinauto.keyboard.send_keys("twitter.com") # adds second tab
-    pywinauto.keyboard.send_keys("{ENTER}")
+import win32gui, win32con
 
 app = application.Application()
-time.sleep(1)
 try:    app.start('C:\\Program Files\\KeePassXC\\KeePassXC.exe')
 except: # Failed
     a = "KeePassXC failed to open!"
 else:  # Success
 
-    time.sleep(2)
+    time.sleep(1)
+    hwnd = win32gui.GetForegroundWindow()
+    win32gui.ShowWindow(hwnd, win32con.SW_MAXIMIZE) 
     pywinauto.mouse.click(button='left', coords=(700, 430))
-    time.sleep(2)
-    pywinauto.keyboard.send_keys("passoword") # type password
+    time.sleep(1)
+    pywinauto.keyboard.send_keys("password") # type password ; side note future me you can remove push's with `git reset --hard hash` and `git push --force`
     pywinauto.mouse.click(button='left', coords=(1100, 590))
+    
+try:    app.start('C:\\Program Files\\Mozilla Firefox\\firefox.exe youtube.com twitter.com')
+except: # Failed
+    a = "Firefox failed to open!"
+else:  # Success
+    a = ""
 
 try:    app.start('C:\\Users\\davon\\AppData\\Roaming\\Spotify\\Spotify.exe')
 except: # Failed
     a = ""
 else:  # Success
     a = ""
-
+    
 try:    app.start('C:\\Users\\davon\\AppData\\Local\\Discord\\Update.exe --processStart Discord.exe')
 except: # Failed
     a = ""
